be2c24293c38b99b6487c3482db495e29d70da57
Chirag <Chirag@CHIRAG-PC>
2018-07-02 17:33:06 -0500

    Add project files.

---
+++ ConsoleApplication34/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seconds = 359999;
+
+            TimeSpan timespan = TimeSpan.FromSeconds(seconds);
+            int day = timespan.Days;
+            int hour = timespan.Hours;
+            int min = timespan.Minutes;
+            int sec = timespan.Seconds;
+            string answer = "";
+
+            if (day > 0)
+            {
+                int daytohour = day * 24;
+                hour = hour + daytohour;
+                answer = hour+":" + timespan.ToString(@"mm\:ss");
+            }
+            else
+            {
+               answer = timespan.ToString(@"hh\:mm\:ss");
+            }
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
+        }
+    }
+}
+
